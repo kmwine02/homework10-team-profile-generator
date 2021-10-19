4ed608013f341aae4aba009ac74ffc7a64fa30b2
Kathleen Wine <katie.wine2@gmail.com>
2021-10-19 18:38:27 -0400

    adding README

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# homework10-team-profile-generator
+Homework 10 - Team Profile Generator 
+  [![License: None]()]()
+  
+ 
+
+  ## Description 
+  This project allows managers to create a team dashboard with information about the different members that make up their team. The manager's name, employee id, email and office phone number will be displayed. For engineers, their name, employee id, email and github username will be displayed. For interns, their name, employee id, email and school will be displayed. This application is run from the command line and dynamically creates an HTML page for the team's dashboard. 
+
+  ## Table of Contents
+  
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+  To install the necessary dependencies, run the following command:
+  npm i
+
+  ## Usage
+  Repo instructions: Once all of the dependencies have been installed, run "node index.js" from the terminal and it will run you through prompts to enter information about your team. 
+
+  ## License
+  This project is licensed under the None license.
+
+  ## Contributing 
+  Contributing is not allowed at this time.
+
+  ## Tests 
+  To run tests, run the following command:
+  npm run test
+
+  ## Questions
+  If you have any questions about the repo, reach out to me directly at katie.wine2@gmail.com. See more of my work at https://www.github.com/kmwine02
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -13,7 +13,7 @@ const generateTeam = (team) => {
         <div class="card-body">
           <ul class="list-group list-group-flush">
             <li class="list-group-item">ID: ${employee.id}</li>
-            <li class="list-group-item">Email: ${employee.email}</li>
+            <li class="list-group-item">Email: <a href="mailto:${employee.email}">${employee.email}</a></li>
             <li class="list-group-item">Office Number: ${employee.officeNumber}</li>
           </ul>
         </div>
@@ -30,9 +30,8 @@ const generateTeam = (team) => {
                   <div class="card-body">
                     <ul class="list-group list-group-flush">
                       <li class="list-group-item">ID: ${employee.id}</li>
-                      <li class="list-group-item">Email: ${employee.email}</li>
-                      <li class="list-group-item">
-                      <a href="https://github.com/${employee.githubUsername}">GitHub: ${employee.githubUsername}</a>
+                      <li class="list-group-item">Email: <a href="mailto:${employee.email}">${employee.email}</a></li>
+                      <li class="list-group-item">GitHub: <a href="https://github.com/${employee.githubUsername} target="_blank">${employee.githubUsername}</a>
                       </li>
                     </ul>
                   </div>
@@ -49,7 +48,7 @@ const generateTeam = (team) => {
         <div class="card-body">
           <ul class="list-group list-group-flush">
             <li class="list-group-item">ID: ${employee.id}</li>
-            <li class="list-group-item">Email: ${employee.email}</li>
+            <li class="list-group-item">Email: <a href="mailto:${employee.email}">${employee.email}</a></li>
             <li class="list-group-item">School: ${employee.school}</li>
           </ul>
         </div>
@@ -67,13 +66,13 @@ const generateHTML = (team) => {
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" type="text/css" href="./style.css" />
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
           crossorigin="anonymous"
         />
+        <link rel="stylesheet" type="text/css" href="./style.css" />
         <title>Team Profile</title>
       </head>
       <body>
